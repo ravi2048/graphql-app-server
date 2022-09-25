c264e8dd5592e63c9d65d00e4b9f2ba07276f55d
Ravi Yadav <rayadav@enphaseenergy.com>
2022-09-25 16:14:03 +0530

    router issue in production

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ app.use('/graphql', graphqlHTTP({
     graphiql: process.env.NODE_ENV === 'development',
 }));
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
 });
