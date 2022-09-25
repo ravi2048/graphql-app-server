08bcbc1dcfd659304ded15c8f8aa2255ce227f62
Ravi Yadav <rayadav@enphaseenergy.com>
2022-09-25 09:53:48 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const { connectDb } = require('./config/db');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config(); //to use env variable here in this file
 const port = process.env.PORT || 5000;
@@ -9,6 +10,9 @@ const port = process.env.PORT || 5000;
 connectDb();
 
 const app = express();
+app.use(cors({
+    origin: '*',
+}));
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: process.env.NODE_ENV === 'development',
